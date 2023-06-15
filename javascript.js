773ee7b84346feeace46d36c7ddf70783de5f638
Pikes Germino <107228395+pikes01@users.noreply.github.com>
2023-06-15 11:56:44 +0800

    code overhaul change from multi links to tab display javascript added

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function openRecipe(evt, recipeName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(recipeName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
